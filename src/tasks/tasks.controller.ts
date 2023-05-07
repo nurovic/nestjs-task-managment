e0dc1ebe9cc39a8374e12580b72652351225246c
nurovic <mertnurovic@gmail.com>
2023-05-07 17:46:17 +0300

    Initilaze Project

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-
+import { Task } from './task.model';
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
   getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks()
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    console.log(title,description)
+    
+    return this.tasksService.createTask(title, description);
   }
 }
+
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class TasksService {
-    private tasks: Task[] = []
+  private tasks: Task[] = [];
 
-    getAllTasks(): Task[] {
-        return this.tasks
-    }
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuidv4(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
 }
