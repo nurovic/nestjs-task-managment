7bff4998de319d768fa77d3ef91a6a0b7b95bafe
nurovic <mertnurovic@gmail.com>
2023-06-04 23:54:53 +0300

    made Validation

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Patch,
   Query,
+  ValidationPipe,
+  UsePipes,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
@@ -36,6 +38,7 @@ export class TasksController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
   }
